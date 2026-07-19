06a5586f94bfb5a29724e13d43c4f069b43d5884
Manuel Barbagallo <you@example.com>
2026-07-19 11:14:07 +0200

    chore: initial project setup with Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Gamedex</h1>
+        <p>Se leggi questo, il progetto parte 🎮</p>
+      </div>
+    </>
+  )
+}
+
+export default App
